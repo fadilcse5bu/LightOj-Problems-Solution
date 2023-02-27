3fb75796571a2803254fb055a9a0bd573f7458f4
Saidul Islam <fadil.cse5.bu@gmail.com>
2023-02-27 09:31:06 +0600

    Update Investigation.cpp

--- 2. Medium/Investigation.cpp
+++ 2. Medium/Investigation.cpp
@@ -1,59 +1,64 @@
+/// How many number of integers in the range [A, B] which are divisible by K and the sum of its digits is also divisible by K.
+
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long int
 
-ll dp[10][2][85][85], len, k;
+ll dp[55][85][85][2][2], k;
+int len = 55;
+string a, b; // fa = flag of lower number(a), fb = flag of upper number(b)
 
-vector <int> dig;
-void convertDigit(int n) {
-  dig.clear();
-  while(n > 0) {
-    dig.push_back(n%10);
-    n /= 10;
+ll cal(int pos, int mod1, int mod2, bool fa, bool fb) {
+  if(pos == len) return (mod1==0 && mod2==0);
+  ll &ret = dp[pos][mod1][mod2][fa][fb];
+  if(ret != -1 && fa && fb) return ret;
+  ret = 0;
+  int low = fa? 0:a[pos]-'0';
+  int high = fb? 9:b[pos]-'0';
+  for(int i = low; i <= high; i++) {
+    ret += cal(pos+1, (mod1*10+i)%k, (mod2+i)%k, fa|(i>low), fb|(i<high));
   }
-  reverse(dig.begin(), dig.end());
-  len = dig.size();
-  //for(int i = 0; i < len; i++) cout << dig[i] << ' ';
-  //cout << endl;
-}
-
-ll calculate(int pos, int tp, int mod1, int mod2) {
-  if(pos == len) {
-    if(mod1 == 0 && mod2 == 0) return 1;
-    return 0;
-  }
-  if(dp[pos][tp][mod1][mod2] != -1) return dp[pos][tp][mod1][mod2];
-  int lim;
-  if(tp == 1) lim = 9;
-  else lim = dig[pos];
-
-  ll res = 0;
-  for(int i = 0; i <= lim; i++) {
-    res += calculate(pos+1, tp|(i < lim ? 1:0), (mod1*10+i)%k, (mod2+i)%k);
-  }
-  return dp[pos][tp][mod1][mod2] = res;
+  return ret;
 }
 
 int main()
 {
   int t=1, cs=1; cin >> t;
   while(t--) {
-    int a, b; cin >> a >> b >> k;
+    cin >> a >> b >> k;
     cout << "Case " << cs++ << ": ";
     if(k > 81) {
       cout << 0 << endl;
       continue;
     }
     memset(dp, -1, sizeof dp);
-    convertDigit(b);
-    int ans = calculate(0,0,0,0);
-    memset(dp, -1, sizeof dp);
-    convertDigit(a-1);
-    ans -= calculate(0,0,0,0);
+    int la = a.size(), lb = b.size();
+    string sa = "", sb = "";
+    for(int i = la; i < len; i++) sa += '0';
+    for(int i = lb; i < len; i++) sb += '0';
+    a = sa + a;
+    b = sb + b;
+    ll ans = cal(0,0,0,0,0);
     cout << ans << endl;
   }
 
   return 0;
 }
+/*
+3
+1 20 1
+1 20 2
+1 1000 4
+
+Case 1: 20
+Case 2: 5
+Case 3: 64
+*/
+
+
+
+
+
+
 
 
